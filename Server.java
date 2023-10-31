ebe557e61d7c1979762cf6f1921724de838fca73
Gustominox <agstcmps.111@gmail.com>
2023-10-31 17:25:53 +0000

    Change how br is instanced

--- Server.java
+++ Server.java
@@ -5,65 +5,57 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
 public class Server {
-    
-    private ServerSocket socket;
 
-    public Server() throws IOException {
+  private ServerSocket socket;
 
-        socket = new ServerSocket(9090);
+  public Server() throws IOException {
+    socket = new ServerSocket(9090);
 
-        while(true){
-            Socket clientSoc = socket.accept();
-            System.out.println("Accepted connection");
+    while (true) {
+      Socket clientSoc = socket.accept();
+      System.out.println("Accepted connection");
 
-            ClientHandler client = new ClientHandler(clientSoc);
+      ClientHandler client = new ClientHandler(clientSoc);
 
-            new Thread(client).start();
+      new Thread(client).start();
+    }
+  }
 
-        }
-        
-        
-        
+  public static class ClientHandler implements Runnable {
+
+    private final Socket client;
+    private BufferedReader br;
+
+    //constructor
+    public ClientHandler(Socket socket) {
+      this.client = socket;
+      try {
+        this.br =
+          new BufferedReader(new InputStreamReader(client.getInputStream()));
+      } catch (IOException exception) {
+        exception.printStackTrace();
+      }
     }
 
-
-    public static class ClientHandler implements Runnable {
-        private final Socket client;
-
-        //constructor
-        public ClientHandler (Socket socket){
-            this.client = socket;
-        }
-
-        public void run() {
-        
-            try{
-                BufferedReader br = new BufferedReader(
-                    new InputStreamReader(client.getInputStream())
-                    );
-
+    public void run() {
+      try {
         // DataOutputStream dOutStream = new DataOutputStream(clientSoc.getOutputStream());
 
-                String recString = br.readLine();
-                System.out.println(recString);
-
-            }catch (IOException e){}
-            finally{}
-            
+        String recString = br.readLine();
+        System.out.println(recString);
         
-        
+      } catch (IOException e) {} finally {
+        try {
+          this.client.close();
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
     }
+  }
 
-
-    }
-    
-
-
-
-    public static void main(String[] args) throws IOException {
-        Server s = new Server();
-    }
-
+  public static void main(String[] args) throws IOException {
+    Server s = new Server();
+  }
 }
