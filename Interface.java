e063c76f2fbc7bf18668c7bb8ad0cae5c19ea355
Beko <carlos.beko.silva@gmail.com>
2023-10-31 16:57:03 +0000

    começo interface?

--- Interface.java
+++ Interface.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class Interface{
-    
+    Scanner s = new Scanner(System.in);
+    int escolha = 0;
+    do{
+
+        System.out.println("1 - Login");
+        System.out.println("2 - Sair");
+        escolha = s.nextInt();
+
+    } while (escolha != 0);
 }
