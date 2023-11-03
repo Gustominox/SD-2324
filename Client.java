81ecb5822d9d37bc1964775485deacb8e9168794
Gustominox <agstcmps.111@gmail.com>
2023-11-03 22:49:43 +0000

    add buffered reader, add send method logic

--- Client.java
+++ Client.java
@@ -1,20 +1,23 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 class Client {
 
   private Socket socket; // = new Socket("Legion", 9090);
+  private BufferedReader br;
+  private BufferedWriter rw;
   private Interface i = new Interface();
 
   public Client() throws IOException {
     socket = new Socket("localhost", 9090);
-    BufferedWriter rw = new BufferedWriter(
-      new OutputStreamWriter(socket.getOutputStream())
-    );
-    rw.write("Hello, world!\n");
-    rw.flush();
+    this.br =
+      new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+    rw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
     System.out.println(socket.getPort());
   }
@@ -26,27 +29,29 @@ class Client {
    * @throws IOException
    */
   public void login() throws IOException {
-
     String username = i.readline();
 
     this.send(username);
-
-
   }
-  
-  /**
-   * 
-   * @param msg
-   */
-  public void send(String msg) {}
 
   /**
-   * 
+   *
+   * @param msg
+   * @throws IOException
+   */
+  public void send(String msg) throws IOException {
+    this.rw.write(msg);
+    this.rw.flush();
+  }
+
+  /**
+   *
    * @param size
    */
   public void recv(int size) {}
 
   public static void main(String[] args) throws IOException {
     Client c = new Client();
+    c.send("HELLO WORLD");
   }
 }
