7bcaa95cb1e4ac7861df61a56b7ef51a763a8885
Gustominox <agstcmps.111@gmail.com>
2023-12-27 16:22:01 +0000

    add thread pool class

---
+++ ThreadPool.java
@@ -0,0 +1,78 @@
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPool {
+
+  private final TaskQueue taskQueue;
+  private final WorkerThread[] workers;
+
+  public ThreadPool(int numThreads) {
+    this.taskQueue = new TaskQueue();
+    this.workers = new WorkerThread[numThreads];
+
+    for (int i = 0; i < numThreads; i++) {
+      workers[i] = new WorkerThread();
+      
+    }
+  }
+
+
+  public void start() {
+    for (int i = 0; i < workers.length; i++) {
+        workers[i].start(); 
+    }
+  }
+
+  public void submitTask(Task task) {
+    taskQueue.addTask(task);
+  }
+
+  public void shutdown() {
+    
+    taskQueue.shutdown();
+    
+    for (WorkerThread worker : workers) {
+      worker.interrupt();
+    }
+  }
+
+  private class WorkerThread extends Thread {
+
+    @Override
+    public void run() {
+      try {
+        while (!Thread.interrupted()) {
+          Task task = taskQueue.getTask();
+          if (task != null) {
+            task.run();
+            taskQueue.reduceMemory(task.getMemory());
+          }
+        }
+      } catch (InterruptedException e) {
+        // Restore the interrupted status
+        Thread.currentThread().interrupt();
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    // Create a thread pool with 3 worker threads
+    ThreadPool threadPool = new ThreadPool(3);
+
+    // Submit some tasks to the thread pool// Example of adding tasks to the queue
+    threadPool.submitTask(new Task("Task 9", 500, 5, new byte[1000]));
+
+    threadPool.submitTask(new Task("Task 8", 200, 5, new byte[1000]));
+    threadPool.submitTask(new Task("Task 1", 200, 0, new byte[1000]));
+    threadPool.submitTask(new Task("Task 2", 200, 0, new byte[1000]));
+    threadPool.submitTask(new Task("Task 3", 200, 0, new byte[1000]));
+    threadPool.submitTask(new Task("Task 4", 200, 0, new byte[1000]));
+    threadPool.submitTask(new Task("Task 5", 200, 0, new byte[1000]));
+    threadPool.submitTask(new Task("Task 6", 200, 0, new byte[1000]));
+    threadPool.submitTask(new Task("Task 7", 200, 5, new byte[1000]));
+    threadPool.submitTask(new Task("Task 10", 200, 5, new byte[1000]));
+
+    // Shutdown the thread pool
+    threadPool.start();
+    threadPool.shutdown();
+  }
+}
