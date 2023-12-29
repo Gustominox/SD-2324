61c0f128ed12230f67e64ab94baf73016bd9454c
Gustominox <agstcmps.111@gmail.com>
2023-12-29 16:38:27 +0000

    Refactor interface code

--- src/InterfaceUser.java
+++ src/InterfaceUser.java
@@ -1,117 +1,134 @@
+import java.io.*;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
-import java.io.*;
 
-public class InterfaceUser{
+public class InterfaceUser {
 
-    static Client c = new Client();
+  private Client c;
+  Scanner scanner;
 
-    public static void main(String[] args) throws IOException{
-        menu1();
+  public InterfaceUser() {
+    c = new Client();
+    scanner = new Scanner(System.in);
+  }
+
+  public String buildMenu1() {
+    StringBuilder menu1 = new StringBuilder();
+    menu1.append("--  LOGIN MENU:  --\n");
+    menu1.append("1. Login\n");
+    menu1.append("2. Register\n");
+    menu1.append("3. Exit\n");
+    menu1.append("Escolha a sua opção: ");
+
+    return menu1.toString();
+  }
+
+  public void print(String input) {
+    System.out.println(input);
+  }
+
+  public void menuInicial() throws IOException {
+    boolean terminado = false;
+
+    while (!terminado) {
+      String menu = buildMenu1();
+      print(menu);
+
+      int opcao = scanner.nextInt();
+      scanner.nextLine();
+      String username;
+      String password;
+      switch (opcao) {
+        case 1:
+          print("--LOGIN--\n");
+          print("Introduza Username: \n");
+          username = scanner.nextLine();
+
+          print("Introduza Password: \n");
+          password = scanner.nextLine();
+
+          if (c.login(username, password)) {
+            menuPrincipal();
+          } else {
+            print("Credencias não correspondem, tente novamente!\n");
+          }
+          break;
+        case 2:
+          print("--REGISTER--\n");
+          print("Introduza Username: \n");
+          username = scanner.nextLine();
+
+          print("Introduza Password: \n");
+          password = scanner.nextLine();
+
+          if (c.registo(username, password)) {
+            menuPrincipal();
+          } else {
+            print("Registo sem Sucesso, tente novamente!\n");
+          }
+          break;
+        case 3:
+          print("Terminar Aplicação!");
+          terminado = true;
+          break;
+        default:
+          print("Escolha Inválida. Introduza opção novamente: ");
+      }
     }
+  }
 
+  public String buildPrincipal() {
+    StringBuilder menu2 = new StringBuilder();
+    menu2.append("--  MAIN MENU:  --");
+    menu2.append("1. Pedir Execução de Um Trabalho\n");
+    menu2.append("2. Consulta de Resultados\n");
+    menu2.append("3. Guardar Resultados num Ficheiro\n");
+    menu2.append("4. Consulta do Estado do Servidor\n");
+    menu2.append("5. Exit\n");
+    menu2.append("Escolha a sua opção: ");
+    return menu2.toString();
+  }
 
-    public static void menu1() throws IOException{
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            StringBuilder menu1 = new StringBuilder();
-            menu1.append("--  LOGIN MENU:  --\n");
-            menu1.append("1. Login\n");
-            menu1.append("2. Register\n");
-            menu1.append("3. Exit\n");
-            menu1.append("Escolha a sua opção: ");
-            System.out.println(menu1.toString());
-            int opcao = scanner.nextInt();
-            scanner.nextLine();
-            switch(opcao){
-                case 1:
-                    menuLogin();
-                    break;
-                case 2:
-                    menuRegist();
-                    break;
-                case 3:
-                    System.out.println("Obrigado, Volte Sempre!");
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Escolha Inválida. Introduza opção novamente: ");
-            }
-        }
+  public void menuPrincipal() {
+    Scanner scanner = new Scanner(System.in);
+    boolean terminado = false;
+    while (!terminado) {
+      String menu = buildPrincipal();
+      print(menu);
+
+      int opcao = scanner.nextInt();
+
+      scanner.nextLine();
+
+      switch (opcao) {
+        case 1:
+          c.pedido(task, tam, code);
+          break;
+        case 2:
+          break;
+        case 3:
+          print("Resultados gravados!\n");
+          break;
+        case 4:
+          c.consulta();
+
+          break;
+        case 5:
+          print("Terminar Aplicação!");
+
+          terminado = true;
+          break;
+        default:
+          print("Escolha Inválida. Introduza opção novamente: ");
+      }
     }
+  }
 
-    public static void menuLogin() throws IOException{
-        Scanner scanner = new Scanner(System.in);
-        StringBuilder loginMenu = new StringBuilder();
-        loginMenu.append("--LOGIN--\n");
-        loginMenu.append("Introduza Username: \n");
-        String username = scanner.nextLine();
-        loginMenu.append("Introduza Password: \n");
-        String password = scanner.nextLine();
-        if (c.login(username, password)) menu2();
-        else System.out.println("Credencias não correspondem, tente novamente!\n");
-    }
-
-
-    public static void menuRegist() throws IOException{
-        Scanner scanner = new Scanner(System.in);
-        StringBuilder registMenu = new StringBuilder();
-        registMenu.append("--REGISTER--\n");
-        registMenu.append("Introduza Username: \n");
-        String username = scanner.nextLine();
-        registMenu.append("Introduza Password: \n");
-        String password = scanner.nextLine();
-        if (c.registo(username, password)) menu2();
-        else System.out.println("Registo sem Sucesso, tente novamente!\n");
-    }
-
-
-
-    public static void menu2(){
-        Scanner scanner = new Scanner(System.in);
-        while (true){
-            StringBuilder menu2 = new StringBuilder();
-            menu2.append("--  MAIN MENU:  --");
-            menu2.append("1. Pedir Execução de Um Trabalho\n");
-            menu2.append("2. Consulta de Resultados\n");
-            menu2.append("3. Guardar Resultados num Ficheiro\n");
-            menu2.append("4. Consulta do Estado do Servidor\n");
-            menu2.append("5. Exit\n");
-            menu2.append("Escolha a sua opção: ");
-            int opcao = scanner.nextInt();
-            scanner.nextLine();
-            switch(opcao){
-                case 1:
-                    
-                    c.pedido(task,tam,code);
-                    break;
-                case 2:
-
-                    
-                    break;
-                case 3:
-                    System.out.println("Resultados gravados!\n");
-                    break;
-                case 4:
-                    c.consulta();
-                    System.out.println();
-                    break;
-                case 5:
-                    System.out.println("Obrigado, Volte Sempre!");
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Escolha Inválida. Introduza opção novamente: ");
-            }
-
-        }
-    }
-
-   /*  public byte[] buscarTarefas(String filePath) {
+  /*  public byte[] buscarTarefas(String filePath) {
         List<byte[]> result = new ArrayList<>();
         try (DataInputStream input = new DataInputStream(new FileInputStream(filePath))){
             String linha;
@@ -135,5 +152,10 @@ public class InterfaceUser{
         }
     }
 */
-}
 
+  public static void main(String[] args) throws IOException {
+    InterfaceUser i = new InterfaceUser();
+
+    i.menuInicial();
+  }
+}
