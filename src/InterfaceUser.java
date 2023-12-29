704aa55074417616191ce98e07e2a3b7806f2f4f
Gustominox <agstcmps.111@gmail.com>
2023-12-29 19:54:18 +0000

    remove unused lines

--- src/InterfaceUser.java
+++ src/InterfaceUser.java
@@ -1,7 +1,4 @@
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class InterfaceUser {
@@ -110,7 +107,6 @@ public class InterfaceUser {
   }
 
   public Boolean menuPrincipal() {
-    Scanner scanner = new Scanner(System.in);
     boolean terminado = false;
 
     try {
